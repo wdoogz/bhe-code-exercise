a9cb26cc2a17deed71ca66e79905c7dc59b1550d
Wesley Dugan <wed30@pitt.edu>
2025-01-29 17:10:20 -0500

    getting prime logic

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,5 +1,33 @@
+import math
 class Sieve:
     def __init__(self) -> None:
-        pass
+        self.prime_array = [] # save array to object attributes for easy referencing
+        self.max_number = 200000000
+        self.pa = self.get_prime()
+
     def nth_prime(self, n: int) -> int:
-        pass
+        prime_array = self.pa
+        print(prime_array[n])
+        return prime_array[n]
+
+    def get_prime(self) -> list:
+
+        # list of bools setting each number to true for every number up to max number+1
+        # boolean lists tend to use less memory than integer lists
+        prime_bool_list = [True for i in range(self.max_number+1)]
+
+        p = 2 # start at 2, 2 is the first prime number
+        # n * n has to be lt or eq max number
+        # We do this to mark each prime number that is divisible by n and gt or eq the square of n
+        while (p*p <= self.max_number):
+            if (prime_bool_list[p]):
+                for i in range(p*p, self.max_number+1, p):
+                    prime_bool_list[i] = False
+            p += 1
+
+        # loop over the potential prime numbers in the max number
+        # to determine prime numbers and append to list.
+        for p in range(2, self.max_number+1):
+            if prime_bool_list[p]:
+                self.prime_array.append(p)
+        return self.prime_array
